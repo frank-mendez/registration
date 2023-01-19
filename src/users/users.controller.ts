eafbaa24f036e0a66e7f3d4ad03d5cebdb42607f
frankmendez <frankmendezresources@gmail.com>
2023-01-19 23:49:55 +0800

    Added initial user CRUD service

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
+import { UpdateUserDto } from './dto/updateUser.dto';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { CreateUserDto } from './dto/createUser.dto';
+import { User } from './users.model';
 import { UsersService } from './users.service';
 
 @Controller('users')
@@ -8,5 +19,28 @@ export class UsersController {
   @Get()
   getAllUsers() {
     return this.userService.getAllUsers();
+  }
+
+  @Post()
+  createUser(@Body() createUserDto: CreateUserDto): User {
+    return this.userService.createUser(createUserDto);
+  }
+
+  @Get('/:id')
+  getUserById(@Param('id') id: string): User {
+    return this.userService.getUserById(id);
+  }
+
+  @Delete('/:id')
+  deleteUserById(@Param('id') id: string): User[] {
+    return this.userService.deleteuserById(id);
+  }
+
+  @Patch('/:id')
+  updateUser(
+    @Param('id') id: string,
+    @Body() updateUserDto: UpdateUserDto,
+  ): User {
+    return this.userService.updateUser(id, updateUserDto);
   }
 }
--- src/users/users.model.ts
+++ src/users/users.model.ts
@@ -1,4 +1,4 @@
-export interface Users {
+export interface User {
   id: string;
   firstName: string;
   lastName: string;
